41ecdf5257fdbc07387e34fdcfe537d3c10f8cd8
Great Joycs <76424765+pavigreatjoycs@users.noreply.github.com>
2023-03-25 13:39:15 +0530

    Update Main.java

--- src/main/java/com/luv2code/pkg/Main.java
+++ src/main/java/com/luv2code/pkg/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World from Main java file");
+		System.out.println("Added second line from github");
 
 	}
 
